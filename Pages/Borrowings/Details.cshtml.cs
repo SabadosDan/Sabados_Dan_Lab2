1589f898d1caadbae67802845812f4d1aedecab4
Sabados Dan <91564800+SabadosDan@users.noreply.github.com>
2024-11-11 21:45:45 +0200

    lab5-finish

---
+++ Pages/Borrowings/Details.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sabados_Dan_Lab2.Data;
+using Sabados_Dan_Lab2.Models;
+
+namespace Sabados_Dan_Lab2.Pages.Borrowings
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Sabados_Dan_Lab2.Data.Sabados_Dan_Lab2Context _context;
+
+        public DetailsModel(Sabados_Dan_Lab2.Data.Sabados_Dan_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public Borrowing Borrowing { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookList = _context.Book.Include(b => b.Author)
+                .Select(x => new
+                {
+                    x.ID,
+                    BookFullName = x.Title + " - " + x.Author.LastName + " " + x.Author.FirstName
+                });
+
+            var borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                .Include(b => b.Member)
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Borrowing = borrowing;
+            }
+            ViewData["BookID"] = new SelectList(bookList, "ID", "BookFullName");
+            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
+            return Page();
+        }
+    }
+}
